5d2abbaec2d80e2c46d97294b4229992ac79b84b
SargisPashayan <pashayansargisl@gmail.com>
2021-07-14 22:53:27 +0400

    feat: added some features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import Button from "./components/Button";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+  }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+
+  decrementCount = () => {
+    this.setState({
+      count: this.state.count - 1,
+    })
+  }
+
+  render() {
+    let { count } = this.state;
+    return (
+      <div className = "counter">
+        <h1>Count: {count}</h1>
+        <Button sign = "+" operation = { () => this.incrementCount() }/>
+        <Button count = {count} sign = "-" operation = { () => this.decrementCount() }/>
+      </div>
+
+    )
+  }
 }
 
 export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 
 class Button extends Component {
   render() {
-    let { title, operation } = this.props;
-    return <button onClick={operation}>{title}</button>;
+    let { sign, count, operation } = this.props;
+    return <button disabled = {count <= 0} onClick={operation}>{sign}</button>;
   }
 }
 
